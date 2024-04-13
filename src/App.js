4deb6b1450872d2532d525cae9edb9bea14d08c7
CHEGEBB <chegephil24@gmail.com>
2024-04-13 15:56:17 +0300

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './index.scss';
+import './components/starter';
+import './components/pro';
+import './components/enterprise';
 
 function App() {
   return (
     <div className="App">
+    <div className="plans">
+
+    </div>
     </div>
   );
 }
