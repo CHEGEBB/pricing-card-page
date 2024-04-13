07044d74ed8c0bb61985ec367fca66932fd45dc6
CHEGEBB <chegephil24@gmail.com>
2024-04-13 15:13:20 +0300

    jsx

---
+++ src/components/pro.js
@@ -0,0 +1,21 @@
+
+const Pro = () => {
+    return ( 
+        <div className="pro">
+            <h2>Pro</h2>
+            <h1>$99/month</h1>
+            <p>2500 MAUs</p>
+            <ul>
+                <li>5000 MAUs</li>
+                <li>10 projects</li>
+                <li>Unlimited guides</li>
+                <li>Unlimited flows</li>
+                <li>Unlimited branded themes</li>
+                <li>Priority email support</li>
+            </ul>
+            <button>Choose Plan</button>
+        </div>
+     );
+}
+ 
+export default Pro;
