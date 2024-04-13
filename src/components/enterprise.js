7e3902fe47f35ed3b7d29e638dec9c63aae1f488
CHEGEBB <chegephil24@gmail.com>
2024-04-13 15:22:22 +0300

    jsx

--- src/components/enterprise.js
+++ src/components/enterprise.js
@@ -5,6 +5,14 @@ const Enterprise = () => {
         <div className="enterprise">
             <h2>Enterprise</h2>
             <h1>Let's Talk!</h1>
+            <ul>
+                <li>All pro features</li>
+                <li>Unlimited MAUs</li>
+                <li>Dedicated environment</li>
+                <li>Unlimited fully customizable themes</li>
+                <li>Custom integrations</li>
+                <li>24/7 support</li>
+            </ul>
         </div>
     );
 }
