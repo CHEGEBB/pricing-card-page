176551d8aa315d69f564fe68d32c7d1a1991f45a
CHEGEBB <chegephil24@gmail.com>
2024-04-13 16:36:08 +0300

    scss

--- src/App.js
+++ src/App.js
@@ -13,16 +13,21 @@ function App() {
 
   return (
     <div className="App">
-    <div className="choice">
-    <h1>Choose Your Plan</h1>
-    </div>
-     
-      <div className={`toggle ${isYearly ? 'yearly' : ''}`} onClick={handleToggle}>
-        <input type="checkbox" id="toggle" className="toggle__input" />
-        <label htmlFor="toggle" className="toggle__label">
-          <div className="toggle__ball"></div>
-        </label>
+      <div className="choice">
+        <h1>Choose Your Plan</h1>
       </div>
+
+      <div className="toggle-container">
+        <span className="toggle-text">Billed monthly</span>
+        <div className={`toggle ${isYearly ? 'yearly' : ''}`} onClick={handleToggle}>
+          <input type="checkbox" id="toggle" className="toggle__input" />
+          <label htmlFor="toggle" className="toggle__label">
+            <div className="toggle__ball"></div>
+          </label>
+        </div>
+        <span className="toggle-text">Billed yearly</span>
+      </div>
+
       <div className="plans">
         <Starter />
         <Pro />
