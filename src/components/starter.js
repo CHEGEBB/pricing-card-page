aa6bb8ea26f67c1db2955ed5ad772e5b5468008c
CHEGEBB <chegephil24@gmail.com>
2024-04-13 15:08:20 +0300

    JSX

---
+++ src/components/starter.js
@@ -0,0 +1,16 @@
+const Starter = () => {
+    return ( 
+        <div className="starter">
+            <h2>Starter</h2>
+            <h1>$19</h1>
+            <ul>
+                <li>500 MAUs</li>
+                <li>3 PROJECTS</li>
+                <li>1GB Storage</li>
+                <li>Unlimited Message</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Starter;
