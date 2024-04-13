ecb8d2a41bdf2abb76cb7c4921f73fdba9095208
CHEGEBB <chegephil24@gmail.com>
2024-04-13 16:19:54 +0300

    scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import Starter from './components/starter';
 import Pro from './components/pro';
 import Enterprise from './components/enterprise';
 
 function App() {
+  const [isYearly, setIsYearly] = useState(false);
+
+  const handleToggle = () => {
+    setIsYearly(!isYearly);
+  };
+
   return (
     <div className="App">
-    <div className="plans">
-    <Starter />
-    <Pro />
-    <Enterprise />
-    
-
-    </div>
+      <h1>Choose Your Plan</h1>
+      <div className={`toggle ${isYearly ? 'yearly' : 'monthly'}`} onClick={handleToggle}>
+        <input type="checkbox" id="toggle" className="toggle__input" checked={isYearly} readOnly />
+        <label htmlFor="toggle" className="toggle__label">
+          <div className="toggle__ball"></div>
+        </label>
+      </div>
+      <div className="plans">
+        <Starter />
+        <Pro />
+        <Enterprise />
+      </div>
     </div>
   );
 }
