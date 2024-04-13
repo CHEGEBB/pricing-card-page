992b0a2b4ce958738d118c8806def2517b193cda
CHEGEBB <chegephil24@gmail.com>
2024-04-13 15:20:52 +0300

    jsx

---
+++ src/components/enterprise.js
@@ -0,0 +1,12 @@
+
+
+const Enterprise = () => {
+    return (  
+        <div className="enterprise">
+            <h2>Enterprise</h2>
+            <h1>Let's Talk!</h1>
+        </div>
+    );
+}
+ 
+export default Enterprise;
